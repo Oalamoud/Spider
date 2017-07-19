b29b5e57e3e2f7d2c63f57ac29babc33006a1eee
oalamoud <lb_silver@hotmail.com>
2017-07-19 13:29:00 +0300

    .

--- domain.py
+++ domain.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
-        return results[-2] + '.' + results[-1]
+        return results[-3] + '.' + results[-2]+ '.' + results[-1]
     except:
         return ''
 
